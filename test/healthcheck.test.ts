c81878f40850f521d313b361b6bbc2449cf81e23
Oksana Kryschuk <kovart.public@gmail.com>
2025-06-21 12:54:01 +0000

    test: update healthcheck.test.ts

---
+++ test/healthcheck.test.ts
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+
+describe("Healthcheck", () => {
+  it("sanity check passes", async () => {
+    // Minimal sanity test to ensure test suite runs reliably
+    expect(true).to.equal(true);
+  });
+});
